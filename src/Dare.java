ce76f72935dd70a0a20d6e4ae8636238a755d3c8
jackykrabs <17jba02@stu.cpsk12.org>
2017-04-21 10:06:14 -0500

    IDK i did something though

--- src/Dare.java
+++ src/Dare.java
@@ -1,4 +1,26 @@
 
 public class Dare {
 
+	String phrase = new String();
+	int difficulty;
+	boolean adult;
+	
+	public Dare(String phrase, int difficulty, boolean adult){
+		this.phrase = phrase;
+		this.difficulty = difficulty;
+		this.adult = adult;
+	}
+	
+	//getters 
+	public String getPhrase(){
+		return phrase;
+	}
+	
+	public int getDifficulty(){
+		return difficulty;
+	}
+	
+	public boolean getAdult(){
+		return adult;
+	}
 }
--- src/DareList.java
+++ src/DareList.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class DareList {
 
+	ArrayList<Dare> dares;
+	
+	public DareList(){
+		//TODO: Create the .json file to hold all the dares, and then query them and load them into this list
+		dares = new ArrayList<Dare>();
+	}
+	
+	//method to return a random dare given the parameters
+	public Dare retrieveDare(int difficulty, boolean isAdult){
+		
+		//go through the full list of dares and pull out the ones that meet the specific criteria
+		ArrayList<Dare> tempDares = new ArrayList<Dare>();
+		for (Dare currentDare: dares) {
+			if(currentDare.getAdult() == isAdult && currentDare.getDifficulty() == difficulty){
+				tempDares.add(currentDare);
+			}
+		}
+		
+		//return a random dare that fits the criteria
+		Random rand = new Random();
+		
+		return tempDares.get(rand.nextInt(tempDares.size()));
+	}
 }
